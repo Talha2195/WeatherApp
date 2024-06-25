dc2a11d56d166f7709ada75f0612d16e2413d5bb
Talha2195 <143935671+Talha2195@users.noreply.github.com>
2024-06-25 13:47:04 +0300

    Dark mode switch
    Added functionality to the darkmode switch button. Now changes text colour for better visibility.

--- src/index/index.js
+++ src/index/index.js
@@ -3,6 +3,8 @@ import { invokeImages } from "../misMods/invokeImagesDom"
 import { slider } from "../slider/sliderDom"
 import { getDetails } from "../displayWeather/fetchDetails"
 import { handleSearch } from "../searchBar/searchBar"
+import day from "../images/dayTime.png"
+import night from "../images/nightTime.png"
 import "./index.css"
 invokeMainPageDom()
 slider()
@@ -12,5 +14,13 @@ const searchInput = document.getElementById("searchBar")
 searchInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     handleSearch(searchInput)
+  }
+})
+const darkModeSwitch = document.getElementById("darkModeSwitch")
+darkModeSwitch.addEventListener("change", function () {
+  if (darkModeSwitch.checked) {
+    document.body.classList.add("darkmode")
+  } else {
+    document.body.classList.remove("darkmode")
   }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    assetModuleFilename: "[name] [ext]",
+    assetModuleFilename: "images/[name][ext]",
   },
   module: {
     rules: [
